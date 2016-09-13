060d594a06c0666ffb0d9b48a9758bd9ab0dd0d9
Markus Hauck <markus1189@gmail.com>
2016-09-13 21:18:23 +0200

    Some finishing touches

--- src/main/scala/de/codecentric/Pokemon.scala
+++ src/main/scala/de/codecentric/Pokemon.scala
@@ -6,6 +6,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 class DriverException extends Throwable // from external library
+class QueryTimedOutException extends DriverException
 
 case class Pokemon(id: Int, typ: Type, name: String)
 object Pokemon {
@@ -48,34 +49,79 @@ object NotFunctional {
 
 object Functional {
   sealed trait TxError extends Product with Serializable
-  case class FindOwnerFailed(owner: PokemonOwner.Id) extends TxError
-  case class FindPokemonFailed(pokemon: Pokemon.Id) extends TxError
-  case class DeletionFailed(id: Pokemon.Id, owner: PokemonOwner, e: Throwable) extends TxError
+  case class Domain(e: DomainError) extends TxError
+  case class External(e: ExternalError) extends TxError
 
-  case class SavingFailed(id: Pokemon, owner: PokemonOwner, e: Throwable) extends TxError
-  case class SavingTimedOut(id: Pokemon.Id, owner: PokemonOwner, e: Throwable) extends TxError
+  sealed trait DomainError
+  case class OwnerNotFound(id: PokemonOwner.Id) extends DomainError
+  case class PokemonNotFound(id: Pokemon.Id) extends DomainError
+
+  sealed trait ExternalError
+  case class FindOwnerFailed(id: PokemonOwner.Id, e: Throwable) extends ExternalError
+  case class FindPokemonFailed(id: Pokemon.Id, e: Throwable) extends ExternalError
+  case class DeletionFailed(id: Pokemon.Id, owner: PokemonOwner.Id, e: Throwable) extends ExternalError
+  case class SavingFailed(id: Pokemon, owner: PokemonOwner.Id, e: Throwable) extends ExternalError
 
   val repo: PokemonRepository = null
   val log = new { def info(s: String) = println(s) }
 
   trait PokemonRepository {
-    def find(id: PokemonOwner.Id): Future[Xor[DriverException,PokemonOwner]]
-    def find(id: Pokemon.Id): Future[Xor[DriverException,Pokemon]]
+    def find(id: PokemonOwner.Id): Future[Xor[DriverException,Option[PokemonOwner]]]
+    def find(id: Pokemon.Id): Future[Xor[DriverException,Option[Pokemon]]]
     def delete(id: Pokemon.Id, owner: PokemonOwner.Id): Future[Xor[DriverException,Unit]]
     def save(p: Pokemon, owner: PokemonOwner.Id): Future[Xor[DriverException,Unit]]
+  }
+
+  def safeFind(id: PokemonOwner.Id): XorT[Future, TxError, PokemonOwner] = {
+    XorT(repo.find(id)).
+      leftMap[TxError](e => External(FindOwnerFailed(id,e))).
+      subflatMap(opt => Xor.fromOption(opt, Domain(OwnerNotFound(id))))
+  }
+
+  def safeFind(id: Pokemon.Id): XorT[Future, TxError, Pokemon] = {
+    XorT(repo.find(id)).
+      leftMap[TxError](e => External(FindPokemonFailed(id,e))).
+      subflatMap(opt => Xor.fromOption(opt, Domain(PokemonNotFound(id))))
+  }
+
+  def safeDelete(id: Pokemon.Id, owner: PokemonOwner.Id): XorT[Future, TxError, Unit] = {
+    XorT(repo.delete(id, owner)).
+      leftMap[TxError](e => External(DeletionFailed(id, owner, e)))
+  }
+
+  def safeSave(p: Pokemon, owner: PokemonOwner.Id) = {
+    XorT(repo.save(p, owner)).
+      leftMap[TxError](e => External(SavingFailed(p, owner, e)))
   }
 
   def transfer(
     from: PokemonOwner.Id,
     to: PokemonOwner.Id)(
     id: Pokemon.Id): Future[Xor[TxError,Unit]] = (for {
-    fromOwner <- XorT(repo.find(from)).leftMap[TxError](_ => FindOwnerFailed(from))
-    toOwner <- XorT(repo.find(to)).leftMap[TxError](_ => FindOwnerFailed(to))
-    pokemon <- XorT(repo.find(id)).leftMap[TxError](_ => FindPokemonFailed(id))
-    _ <- XorT(repo.delete(id, from)).leftMap[TxError](DeletionFailed(id, fromOwner, _))
-    _ <- XorT(repo.save(pokemon, to)).leftMap[TxError](SavingFailed(pokemon, toOwner, _))
+    fromOwner <- safeFind(from)
+    toOwner <- safeFind(to)
+    pokemon <- safeFind(id)
+    _ <- safeDelete(id,from)
+    _ <- safeSave(pokemon, to)
   } yield {
     log.info(
       s"Transferred pokemon ${pokemon.name}: ${fromOwner.name} -> ${toOwner.name}")
   }).value
+
+  val o1 = PokemonOwner.Id(1)
+  val o2 = PokemonOwner.Id(2)
+  val p = Pokemon.Id(3)
+
+  transfer(o1,o2)(p) map {
+    case Xor.Right(()) => """\o/"""
+    case Xor.Left(Domain(domain)) => domain match {
+      case OwnerNotFound(id) =>   // display error
+      case PokemonNotFound(id) => // display error
+    }
+    case Xor.Left(External(external)) => external match {
+      case SavingFailed(id, owner, e: QueryTimedOutException) => // UH OH! Retry?
+      case SavingFailed(_,_,e) => // log error and display generic error
+      case _ => // some generic handling
+    }
+  }
 }
